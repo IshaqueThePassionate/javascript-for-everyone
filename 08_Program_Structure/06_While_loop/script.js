32460eb131130e3ed47bd90f7b51ef190c7e2864
CodingSahib <muhammadishaque732@gmail.com>
2024-08-20 14:56:08 +0500

    While loop

--- 08_Program_Structure/06_While_loop/script.js
+++ 08_Program_Structure/06_While_loop/script.js
@@ -1,46 +1,46 @@
 
-// let n = 0;
+let n = 0;
 
-// while (n < 3) {
-//   n++;
-// }
+while (n < 3) {
+  n++;
+}
 
-// console.log(n);
+console.log(n);
 // Expected output: 3
 
 
 // --------------------------------------------------------------------------------------------------------------------------------
 
 
-// let n = 0;
-// let x = 0;
+let m = 0;
+let x = 0;
 
-// while (n < 3) {
-//   n++;
-//   x += n;
-//   console.log(n);
+while (m < 3) {
+  n++;
+  x += n;
+  console.log(m);
   
-// }
+}
 
 // --------------------------------------------------------------------------------------------------------------------------------
 
 
 // Example : Sum of Only Positive Numbers
 
-// let num = 0, sum = 0;
+let num = 0, sum = 0;
 
 // loop as long as num is 0 or positive
-// while (num >= 0) {
+while (num >= 0) {
 
     // add all positive numbers
-    // sum += num;
+    sum += num;
 
     // take input from the user
-//   num = parseInt(prompt("Enter a number: "));
-// }
+  num = parseInt(prompt("Enter a number: "));
+}
 
 // last, display sum
-// console.log(`The sum is ${sum}`);
+console.log(`The sum is ${sum}`);
 
 // Output
 
@@ -56,16 +56,16 @@
 // Initialize the array
 
 
-// const friends = ["Ahmed", "Sara", "Zain", "Fatima", "Ali"];
+const friends = ["Ahmed", "Sara", "Zain", "Fatima", "Ali"];
 
 // Initialize the loop counter
-// let i = 0;
+let i = 0;
 
-// // Use while loop to iterate through the array
-// while (i < friends.length) {
-//     console.log(friends[i]);
-//     i++; // Increment the counter
-// }
+// Use while loop to iterate through the array
+while (i < friends.length) {
+    console.log(friends[i]);
+    i++; // Increment the counter
+}
 
 // // Output
 
@@ -82,18 +82,18 @@
 
 // Initialize the array of first names
 
-// const friends = ["Ahmed", "Sara", "Zain", "Fatima", "Ali"];
+const friendss = ["Ahmed", "Sara", "Zain", "Fatima", "Ali"];
 
 // Initialize the loop counter
-// let i = 0;
+let j = 0;
 
 // Add last name "Khan" in the loop's body and print full names
 
-// while (i < friends.length) {
-//     const fullName = friends[i] + " Khan"; // Combine first name with last name
-//     console.log(fullName);
-//     i++; // Increment the counter
-// }
+while (j<friends.length) {
+    const fullName = friends[j] + " Khan"; // Combine first name with last name
+    console.log(fullName);
+    j++; // Increment the counter
+}
 
 // output
 
