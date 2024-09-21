48a874d6533fe6e09a184ccc772d77325260ff0f
CodingSahib <muhammadishaque732@gmail.com>
2024-09-21 14:54:42 +0500

    Arithematic Operator

---
+++ 05_Operators_in_Javascript/01_Artihmetic_Operators/script.js
@@ -0,0 +1,49 @@
+
+// Addition (+)
+
+let sum = 5 + 3;
+console.log(sum);  // Output: 8
+
+
+// Subtraction (-)
+
+let difference = 10 - 4;
+console.log(difference);  // Output: 6
+
+
+// Multiplication (*)
+
+let product = 6 * 7;
+console.log(product);  // Output: 42
+
+
+// Division (/)
+
+let quotient = 20 / 4;
+console.log(quotient);  // Output: 5
+
+
+// Modulus (remainder) (%)
+
+let remainder = 17 % 3;
+console.log(remainder);  // Output: 2
+
+
+// Exponentiation (**)
+
+let power = 2 ** 3;
+console.log(power);  // Output: 8
+
+
+// Increment (++)
+
+let counter = 5;
+counter++;
+console.log(counter);  // Output: 6
+
+
+// Decrement (--)
+
+counter--;
+console.log(counter);  // Output: 5
+
